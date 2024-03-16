e04bd9d6d45783bc7cc3c6bf4d44c9c317755c84
nlemuel <lemuelayres@gmail.com>
2024-03-16 17:16:42 -0300

    add counter

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # opencv-studysession
-Repo com o objetivo de estudar um pouco mais sobre visão computacional com Python utilizando a biblioteca opencv
+Repositório com o objetivo de estudar um pouco mais sobre visão computacional com Python utilizando a biblioteca opencv
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import cv2
 import pandas as pd
 import numpy as np
 from ultralytics import YOLO
+from tracker import *
 
 
+list = []
 
 model=YOLO('yolov8s.pt')
 
@@ -23,6 +25,11 @@ my_file = open("coco.txt", "r")
 data = my_file.read()
 class_list = data.split("\n")
 count=0
+
+area = [(270, 238), (285, 262), (592, 226), (552, 207)]
+tracker = Tracker()
+area_c = set()
+
 while True:
     
     ret,frame = cap.read()
@@ -32,6 +39,8 @@ while True:
     if count % 3 != 0:
         continue
     frame=cv2.resize(frame,(1020,500))
+
+    list.clear()
 
     results = model.predict(frame)
 
@@ -51,10 +60,27 @@ while True:
     # Desenhar as caixas delimitadoras e as classes no frame
     for index, row in df.iterrows():
         x1, y1, x2, y2, c = row
-        cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255,191,117), 2)
-        cv2.putText(frame, str(c), (int(x1), int(y1)), cv2.FONT_HERSHEY_COMPLEX, 0.7, (117,158,255), 1)
 
+        if 'car' in c:
+            list.append([x1, y1, x2, y2])
+    bbox_id = tracker.update(list)
+    for bbox in bbox_id:
+            x3, y3, x4, y4, id = bbox
+            cx = int(x3 + x4) // 2
+            cy = int(y3 + y4) // 2
+            results = cv2.pointPolygonTest(np.array(area, np.int32), ((cx, cy)), False)
+
+            if results >= 0:
+                cv2.circle(frame, (cx, cy), 4, (255,191,117), -1)
+                cv2.rectangle(frame, (int(x3), int(y3)), (int(x4), int(y4)), (255,191,117), 2)
+                cv2.putText(frame, str(id), (int(x3), int(y3)), cv2.FONT_HERSHEY_COMPLEX, 0.7, (117,158,255), 1)
+                area_c.add(id)
+
+    cv2.polylines(frame, [np.array(area, np.int32)], True, (255,164,58), 3)
+    count= len(area_c)
+    cv2.putText(frame, str(count), (67, 147), cv2.FONT_HERSHEY_COMPLEX, 2, (117,158,255), 1)
     cv2.imshow("RGB", frame)
+
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
